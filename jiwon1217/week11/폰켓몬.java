bda986307f387a23356a8ac930b3d6a49b11031a
jiwon1217 <koagjw@gmail.com>
2025-06-16 09:05:06 +0900

    :white_check_mark: 불필요한 코드 삭제

--- jiwon1217/week11/폰켓몬.java
+++ jiwon1217/week11/폰켓몬.java
@@ -13,12 +13,10 @@ public class 폰켓몬 {
      * O(N)
      */
     public int solution(int[] nums) {
-        int answer = 0;
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-        answer = Math.min(set.size(), nums.length / 2);
-        return answer;
+        return Math.min(set.size(), nums.length / 2);
     }
 }
